cceed83d8d76788c79cca67e691b6ff4394d9b39
radoslawglowacki <radglo1995@gmail.com>
2021-07-12 20:54:19 +0200

    implementation download image on frontend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -19,7 +19,10 @@ const UserProfiles = () => {
 
   return userProfiles.map((userProfile, index) => {
     return(<div key={index}>
-      {/*profile image*/}
+      {userProfile.userProfileId ? (
+          <img
+          src={`http://localhost:8084/api/v1/user-profile/${userProfile.userProfileId}/image/download`}/>) : null}
+
       <br/>
       <br/>
       <h1>{userProfile.username}</h1>
