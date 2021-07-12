599bc7c998fff6ff6179d38f7c4338da05554188
radoslawglowacki <radglo1995@gmail.com>
2021-07-12 14:43:40 +0200

    add user profile

--- src/main/java/com/example/aws/profile/UserProfile.java
+++ src/main/java/com/example/aws/profile/UserProfile.java
@@ -1,7 +1,12 @@
 package com.example.aws.profile;
 
-import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.Objects;
+import java.util.UUID;
+@Getter
+@Setter
 public class UserProfile {
 
     private UUID userProfileId;
@@ -12,5 +17,19 @@ public class UserProfile {
         this.userProfileId = userProfileId;
         this.username = username;
         this.profileImageLink = profileImageLink;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserProfile that = (UserProfile) o;
+        return Objects.equals(userProfileId, that.userProfileId) && Objects.equals(username, that.username)
+                && Objects.equals(profileImageLink, that.profileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, username, profileImageLink);
     }
 }
